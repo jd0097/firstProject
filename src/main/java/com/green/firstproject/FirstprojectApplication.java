18677249f04c3e7aaa588752c9d4640886d8c264
jd0097 <131757796+jd0097@users.noreply.github.com>
2023-07-27 20:31:03 +0900

    Update FirstprojectApplication.java

--- src/main/java/com/green/firstproject/FirstprojectApplication.java
+++ src/main/java/com/green/firstproject/FirstprojectApplication.java
@@ -3,6 +3,9 @@ package com.green.firstproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableScheduling
 @SpringBootApplication
@@ -11,5 +14,17 @@ public class FirstprojectApplication {
     public static void main(String[] args) {
         SpringApplication.run(FirstprojectApplication.class, args);
     }
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
+						.allowedMethods("*") // 어떤 메서드를 허용할 것인지 (GET, POST…)
+						.allowedOrigins("https://web-travel-todo-cu6q2blkafu2i7.sel4.cloudtype.app");
+			}
+		};
+	}
 
 }
