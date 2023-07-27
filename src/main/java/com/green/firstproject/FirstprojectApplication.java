e38ee7211c192a29ad836a7c827f6443fa72d303
jd0097 <131757796+jd0097@users.noreply.github.com>
2023-07-27 21:45:59 +0900

    Update FirstprojectApplication.java

--- src/main/java/com/green/firstproject/FirstprojectApplication.java
+++ src/main/java/com/green/firstproject/FirstprojectApplication.java
@@ -22,7 +22,7 @@ public class FirstprojectApplication {
 				registry.addMapping("/**")
 						.allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
 						.allowedMethods("*") // 어떤 메서드를 허용할 것인지 (GET, POST…)
-						.allowedOrigins("https://web-travel-todo-cu6q2blkafu2i7.sel4.cloudtype.app");
+						.allowedOrigins("*");
 			}
 		};
 	}
